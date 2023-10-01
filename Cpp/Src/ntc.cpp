a539935fa8fe64e9002e64817b0291e5310dfa2c
Andrew Abramow <andrewabramow@gmail.com>
2023-10-01 11:28:55 +0300

    NTC tested

--- Cpp/Src/ntc.cpp
+++ Cpp/Src/ntc.cpp
@@ -11,10 +11,10 @@
 NTC::NTC() {
 	b_ = 3495;          // temperature coefficient
 	t_ = 25;            // normal temperature
-	Rt_ = 10000;        // resistance at normal temperature
+	Rt_ = 11000;        // resistance at normal temperature
 	R_ = 10000;         // additional resistance
 	resolution_ = 12;   // blue pill ADC - 12 bit
-	analog_val_ = 2.0f; // default value
+	analog_val_ = 0.0f; // default value
 }
 
 NTC::~NTC() {
@@ -24,12 +24,12 @@ NTC::~NTC() {
 uint16_t NTC::ReadAnalogNTC(ADC_HandleTypeDef hadc1) {
 	HAL_ADC_Start(&hadc1);
 	analog_val_ = (float)HAL_ADC_GetValue(&hadc1);
-	//HAL_ADC_Stop(&hadc1);
+	HAL_ADC_Stop(&hadc1);
 	return (float)analog_val_;
 }
 
 float NTC::NTC_Compute() {
-	analog_val_ = ((float)(R_/Rt_)) / ((float)((1<<resolution_) - 1) / analog_val_ - 1.0f);
+	analog_val_ = 0.9f/((float)(4096 - 1)/analog_val_ - 1.0f);
 	analog_val_ = (log(analog_val_) / b_) + 1.0f / (t_ + 273.15f);
 	return (1.0f / analog_val_ - 273.15f);
 }
